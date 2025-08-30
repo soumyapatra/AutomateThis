720da3bc26451ccd503730aa6aa69142faaa62e1
soumyapatra <soumyapatra9403@gmail.com>
2025-08-30 11:13:16 +0530

    Add terraform code for azure

---
+++ TERRAGRUNT/AZURE/modules/aks/main.tf
@@ -0,0 +1,27 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "aks" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  dns_prefix = var.dns_prefix
+}
+
---
+++ TERRAGRUNT/AZURE/modules/aks/variables.tf
@@ -0,0 +1,19 @@
+variable "cluster_name" {
+  type = string
+}
+
+variable "node_count" {
+  type = string
+}
+
+variable "dns_prefix" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
